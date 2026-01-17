7b7ee21d332d538053c2c5645fd218341a21f478
Paweł Śliwiński <psliwinski@Pawes-MacBook-Pro.local>
2026-01-17 08:36:16 +0100

    Remove unused JavaScript file and cleanup

--- js/lessons.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-const data = fs.readFileSync("data/js_lessons.json", "utf-8");
-const lessons = JSON.parse(data);
-
-console.log("Lekcje JavaScript:");
-lessons.forEach(l => {
-  console.log(l.id + ". " + l.title);
-});
